42fb6b0372ded027999a08590ea962cb2a95af42
AwoOsaLoni <al558019@edu.uaa.mx>
2026-04-12 23:51:50 -0600

    Update model_training.py

--- notebooks/model_training.py
+++ notebooks/model_training.py
@@ -6,10 +6,8 @@ from sklearn.metrics import mean_absolute_error, r2_score
 import joblib
 import os
 
-# Asegúrate de cargar el dataset que limpiamos en el paso anterior
 df = pd.read_csv('esports_cleaned .v2.csv')
 
-# 2. SELECCIÓN ÓPTIMA (Las que el modelo "no conocía" antes)
 features_esports = [
     'hours_played_weekly', 
     'win_rate', 
@@ -21,13 +19,11 @@ features_esports = [
 X = df[features_esports]
 y = df['monthly_revenue_usd']
 
-# 3. Entrenar el nuevo modelo
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X, y)
 
-# 4. Guardar los nuevos artefactos (Sobreescribiendo los viejos)
 if not os.path.exists('models'): os.makedirs('models')
 joblib.dump(model, 'models/random_forest_esports.pkl')
 joblib.dump(features_esports, 'models/feature_names.pkl')
 
-print("✅ Modelo regenerado con éxito con columnas de rendimiento de juego.")
+print("✅ Modelo generado con éxito con columnas de rendimiento de juego.")
